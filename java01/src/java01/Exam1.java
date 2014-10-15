2464c2aa8a510874d8d4899c122ac91c07af99f8
jiyeong <jiyeong.kim5840@gmail.com>
2014-10-15 09:09:41 +0900

    .

---
+++ java01/src/java01/Exam1.java
@@ -0,0 +1,21 @@
+package java01;
+
+public class Exam1 {
+
+  public static void main(String[] args) {
+
+    /*    
+    int a = 17;
+    int a2 = 015; //001 101_13
+    int a3 = 0xF; //0000 1111_15
+    int a4 = 006; // 000 110_6
+    
+    System.out.println(a+a2-a3+a4);
+    System.out.println(a3+a4);
+    */
+    
+    
+
+  }
+
+}
