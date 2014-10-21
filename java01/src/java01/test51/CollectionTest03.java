b94e3eb4b277f137ecbb3a80a285418fe43ab9bf
jiyeong <jiyeong.kim5840@gmail.com>
2014-10-21 20:57:51 +0900

    .

--- java01/src/java01/test51/CollectionTest03.java
+++ java01/src/java01/test51/CollectionTest03.java
@@ -23,6 +23,8 @@ class MyArray2{
 
   public int remove(int pos) { // 앞으로 땡기기
     if (pos >= 0 && pos < this.cursor){
+      System.out.println("Remove-----------------------");
+      System.out.println("list["+pos+"]에 값"+list[pos]+"을 Remove한다.");
       for(int i = pos; i < this.cursor;i++){
         if(i == (this.cursor - 1)){
           list[i] = null;
@@ -40,20 +42,22 @@ class MyArray2{
 
   public int insert(int pos, String value){ //추가하기
     if (pos >= 0 && pos < size()){
+      System.out.println("Insert-----------------------");
       for(int i = this.cursor ; i >= pos ;i--){
         if(i > pos){
           list[i] = null;
           list[i] = list[i-1];
         } else if(i == pos){
           list[i] = value;
+          System.out.println("list["+i+"]에 값"+value+"을 Insert한다.");
           this.cursor++;
         }//if
       }//for
-    return 0;
-  } else {
-    return -1;
-  } //if
-}
+      return 0;
+    } else {
+      return -1;
+    } //if
+  }
 }
 
 public class CollectionTest03 {
@@ -69,15 +73,24 @@ public class CollectionTest03 {
     System.out.println(arr.add("66666"));
     System.out.println(arr.add("77777"));
 
+    int count = arr.size(); //초기 값 개수
 
-    System.out.println("--------------------");
+    System.out.println("초기값--------------------");
     for (int i = 0; i < arr.size(); i++){
-      System.out.println(arr.get(i)); //get을 통해 주소 꺼냄
+      System.out.println("list["+i+"]"+arr.get(i)); //get을 통해 주소 꺼냄
     } //for
 
     arr.remove(3);
-
-    System.out.println("--------------------");
+    arr.remove(4);
+    
+    int REcount = arr.size();//제거 후 남은 개수
+    int Rcount = count - arr.size(); // 제거한 개수
+    if(REcount == 0) 
+      System.out.println("더이상 제거할 값이 없습니다.");
+    //System.out.println(arr.size());
+    //System.out.println(arr.cursor);
+    
+    System.out.println("R----------------------------");
     for (int i = 0; i < arr.size(); i++){
       System.out.println("list["+i+"]"+arr.get(i)); //get을 통해 주소 꺼냄
     } //for
@@ -86,15 +99,24 @@ public class CollectionTest03 {
     //arr.insert(4, "xxxxx");
     arr.insert(5, "xxxxx");
     arr.insert(6, "xxxxx");
-    //arr.insert(7, "xxxxx");
+    arr.insert(7, "xxxxx");
     //arr.insert(9, "xxxxx");
+    
+    int IEcount = arr.size(); // 제거와 추가 후 전체 개수
+    int Icount = arr.size() - REcount; // 추가한 개수
 
+    if(IEcount == 10) 
+      System.out.println("더이상 추가할 공간이 없습니다.");
+    
+    System.out.println(count +","+ Rcount +","+ Icount+","+REcount +","+ IEcount);
+    
 
-    System.out.println("--------------------");
+    
+    
+    System.out.println("I----------------------------");
     for (int i = 0; i < arr.size(); i++){
       System.out.println("list["+i+"]"+arr.get(i)); //get을 통해 주소 꺼냄
     } //for
-    
   }
 
 }
