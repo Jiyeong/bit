99ffada645caf284416ee99db92fef8fea89cd9b
jiyeong <jiyeong.kim5840@gmail.com>
2014-11-03 21:01:37 +0900

    .

---
+++ example/src/november3/Ex01.java
@@ -0,0 +1,30 @@
+package november3;
+
+
+public class Ex01 {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+/*
+annotation?!?!?!?!?!
+cf) doclet / JCP, JSR
+SOURCE CLASS RUNTIME
+나중에 값을 추출해서 사용하려고 쓰는 주석
+
+
+Set<Class<?>> clazzList = reflections.getTypesAnnotatedWith(Component.class);
+Set 
+generic -> 일반화
+?-> 클래스 타입
+자동 타입캐스팅함. 타입캐스팅 안해도됨.
+
+
+collection
+arry
+list
+set(집합) list와 같다 BUT 중복 허용안함
+map
+*/
+  }
+
+}
