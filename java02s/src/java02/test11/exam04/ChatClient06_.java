eff58402b11902669773237ef2937575dd9eafe9
bit <wkdeogur0677@naver.com>
2014-11-07 08:38:19 +0900

    11.06

---
+++ java02s/src/java02/test11/exam04/ChatClient06_.java
@@ -0,0 +1,103 @@
+// 우리는 프레임을 만들고시프다
+package java02.test11.exam04;
+
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.FlowLayout;
+import java.awt.Frame;
+import java.awt.Label;
+import java.awt.Panel;
+import java.awt.TextArea;
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+
+public class ChatClient  extends Frame implements ActionListener { // 윈도우가 사용하여 만듬!(OS함수를 통해서)
+  TextField tfServerAddr = new TextField(20);
+  TextField tfName = new TextField(10);
+  Button btnConnect = new Button("연결");
+  TextArea taContent = new TextArea();
+  TextField tfInput = new TextField(30);
+  Button btnSend = new Button("보내기");
+
+  String username;
+  String serverAddress;
+  
+  public ChatClient() {
+    // 윈도우 준비
+    Panel toolbar = new Panel(new FlowLayout(FlowLayout.LEFT));
+    toolbar.add(new Label("이름 : "));
+    toolbar.add(tfName);
+    toolbar.add(new Label("서버 : "));
+    toolbar.add(tfServerAddr);
+    toolbar.add(btnConnect); 
+    
+    this.add(toolbar, BorderLayout.NORTH);
+    this.add(taContent, BorderLayout.CENTER);
+
+    Panel bottom = new Panel();
+    bottom.add(tfInput);
+    bottom.add(btnSend);
+
+    this.add(bottom, BorderLayout.SOUTH);
+
+    // 리스너 등록
+    // 1) 윈도우 이벹르를 처리할 리스너 객체 등록
+    // WindowListener 인터페이스를 구현한 객체이어야 한다.
+    this.addWindowListener(new WindowAdapter() {
+      @Override
+      public void windowClosing(WindowEvent e) {
+        System.exit(0);
+      }
+    });
+    
+
+    // ActionEvent는 버튼을 눌렀을 때 발생하는 이벤트이다.
+    // connectBtn.addActionListener(new MyConnectListener());
+
+    // 실무에서는 한 번 밖에 안 쓸 객체라면 익명 이너 클래스로 정의한다.
+    // 바로 직관적 코드의 가독성이 좋아진다.
+    btnConnect.addActionListener(this);
+
+    // 보내기 버튼을 눌렀을 때 발생하는 이벤트이다.
+    // sendBtn.addActionListener(new MySendListener());
+    btnSend.addActionListener(this);
+  }
+
+
+  public static void main(String[] args) {
+    ChatClient wnd = new ChatClient();
+    wnd.setSize(400, 600);
+    wnd.setVisible(true);
+
+  }
+
+
+  @Override
+  public void actionPerformed(ActionEvent e) {
+    if(e.getSource() == btnConnect) { //연결 버튼을 눌렀다면,
+      username = tfName.getText();
+      serverAddress = tfServerAddr.getText();
+      System.out.println("사용자 이름 : " + username);
+      System.out.println("서버 주소 : " + serverAddress);
+      
+    } else { // 보내기 버튼을 눌렀다면
+      
+    }
+    
+  }
+
+  /*  // WindowListener를 직접 구현하지 말고,
+  // 미리 구현한 WindowAdapter를 상속 받아라!
+  class MyWindowListener extends WindowAdapter {
+    // 다음 메서드는 윈도우에서 close 버튼을 눌렀을 때 호출된다.
+    @Override
+    public void windowClosing(WindowEvent e) {
+      System.exit(0);
+    }
+  }*/
+}
+
